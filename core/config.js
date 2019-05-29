1bec7f50a5c57df794b8457df7eaa0b635645bb5
outpexgames <50186369+outpexgames@users.noreply.github.com>
2019-05-28 19:26:06 -0700

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || '',
-    BOT_TOKEN: process.env.BOT_TOKEN || '',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://jerry:<password>@bot-pgvbr.gcp.mongodb.net/test?retryWrites=true',
+    BOT_TOKEN: process.env.BOT_TOKEN || 'NTgzMTA1MjAzMTcyMDE2MTI4.XO3tpw.bdQ1ARhL5SCoDgNaCzFBNdMRKX4',
     SUDO: process.env.SUDO || '',
     LOG_CHANNEL: process.env.LOG_CHANNEL || ''
 };
